285b640f239794f3a5826c184dd2124365ad3522
Dreemerrr <khareanant05@gmail.com>
2025-12-26 15:36:52 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,37 @@
+print ("Welcome to the Calculator Program!")
+print ("You can perform addition, subtraction, multiplication, and division.")
+print ("Type 'exit' to end the program.")
+print ("1. Addition (+)")
+print ("2. Subtraction (-)")
+print ("3. Multiplication (*)")
+print ("4. Division (/)")
+
+option = int(input("Please select an operation (1/2/3/4): "))
+if option in (1, 2, 3, 4):
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    if option == 1:
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+
+    elif option == 2:
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+
+    elif option == 3:
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+
+    elif option == 4:
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("Error: Division by zero is not possible.")
+else:
+    print("Invalid input. Please select a valid operation.")
+
+if option == 'exit':
+    print("Exiting the program. Goodbye!")
+            
